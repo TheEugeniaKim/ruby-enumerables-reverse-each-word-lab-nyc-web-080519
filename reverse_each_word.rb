79855d6d043f316402e3cf284e546c8a3870f31c
Eugenia Kim <elk303@nyu.edu>
2019-07-27 02:13:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -26,15 +26,3 @@ end
 
 
 
-
-# def reverse_each_word(string)
-#   newarray = string.split
-#   reverse_each_word_array = newarray.each{ |word| word.reverse!} 
-#   return reverse_each_word_array.join(" ")
-# end
-
-# def reverse_each_word(string)
-#   newarray = string.split
-#   reverse_each_word_array = newarray.collect{ |word| word.reverse!} 
-#   return reverse_each_word_array.join(" ")
-# end
